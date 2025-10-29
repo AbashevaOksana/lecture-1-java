9d0dbdfbdce3f3a67d722ba2dd0d911bb3006243
Abasheva Oksana <antona9@ya.ru>
2025-10-30 01:00:09 +0400

    add Lessons1Java 2

--- src/test/java/Lessons1Java.java
+++ src/test/java/Lessons1Java.java
@@ -56,6 +56,7 @@ public class Lessons1Java {
             int f = 5;
             int j = 10;
 
+
             System.out.println("i < f: " + (i < f));   // false
             System.out.println("i > f: " + (i > f));   // true
             System.out.println("i <= j: " + (i <= j)); // true
